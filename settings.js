c994daad9dc01758519d90f913ab966e967a5f2b
sushakps <128962966+sushakps@users.noreply.github.com>
2023-04-23 16:23:08 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -55,7 +55,7 @@ global.mess = {
     group: 'Features Used Only For Groups!',
     private: 'Features Used Only For Private Chat!',
     bot: 'This feature could be used by bot only',
-    wait: 'ᴘʟᴇᴀꜱᴇ ᴡɪᴛʜ...',
+    wait: 'ᴘʟᴇᴀꜱᴇ Wait...',
     linkm: 'Where is the link?',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
     nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
